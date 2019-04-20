dd22d7fe0ddbc6e92afe276af386bb64db5926ec
Григорьев Юрий <trinus21@mail.ru>
2019-04-20 10:48:38 +0300

    lr3

--- lab3/firstwebpage/firstwebpage/settings.py
+++ lab3/firstwebpage/firstwebpage/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'flatpages'
+    'flatpages',
+    'articles',
 ]
 
 MIDDLEWARE = [
